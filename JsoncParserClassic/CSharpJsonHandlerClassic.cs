3ab8896b5d27aab424ae5d006078d1124af403ff
JavaCommons Technologies <javacommons@gmail.com>
2026-01-20 18:18:59 +0900

    JsoncParserClassic-v2026.0120.1818.43

--- JsoncParserClassic/CSharpJsonHandlerClassic.cs
+++ JsoncParserClassic/CSharpJsonHandlerClassic.cs
@@ -1,19 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-// ReSharper disable once CheckNamespace
+﻿// ReSharper disable once CheckNamespace
 namespace Global;
 
 public class CSharpJsonHandlerClassic: IJsonHandlerClassic
 {
     private readonly JsoncParserClassic jsonParser;
-    private readonly ObjectParserClassic objParser;
+    private readonly PlainObjectConverter objParser;
     // ReSharper disable once ConvertToPrimaryConstructor
     public CSharpJsonHandlerClassic(bool numberAsDecimal, bool forceAscii)
     {
         this.jsonParser = new JsoncParserClassic(numberAsDecimal);
-        this.objParser = new ObjectParserClassic(forceAscii);
+        this.objParser = new PlainObjectConverter(forceAscii);
     }
     public object Parse(string json)
     {
